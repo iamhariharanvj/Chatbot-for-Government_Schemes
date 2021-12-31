43f7f0a4bcf9f6be699da09e4f6bb87a7f818b9b
iamhariharanvj <hariharanvj2003@gmail.com>
2021-12-31 11:10:38 +0530

    final commits

--- api.py
+++ api.py
@@ -2,6 +2,12 @@ import flask
 from chat import bot_response
 from flask import Flask, render_template, request
 import webbrowser
+from datetime import datetime, time
+
+now = datetime.now()
+
+current_time = now.strftime("%H:%M")
+
 
 
 app = Flask(__name__,template_folder="./")
@@ -9,12 +15,12 @@ app.static_folder = 'static'
 
 @app.route("/dark")
 def chat():
-    return render_template("dark theme.html")
+    return render_template("dark theme.html",time=current_time)
 
 @app.route("/")
 def home():
     
-    return render_template("index.html")
+    return render_template("index.html",tym=current_time)
 
 @app.route("/get")
 def get_bot_response():
